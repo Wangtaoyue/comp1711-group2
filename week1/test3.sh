5bee854d1ea4bf94ac64fb1ce71c062e3a6ca89e
Wangtaoyue <146682567+Wangtaoyue@users.noreply.github.com>
2023-11-08 16:59:13 +0000

    work

---
+++ week1/test3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "What is your name? " name
+
+read -p "Where are you from? " location
+
+read -p "What is your favorite color? " color
+
+echo "Nice to meet you, $name! So, you're from $location, and your favorite color is $color."
+
